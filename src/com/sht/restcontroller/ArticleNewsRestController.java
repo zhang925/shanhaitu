787ebac8fbbe3d6a3688be1858a231806f35ba32
zhang925 <zhang925518@163.com>
2018-06-04 23:53:17 +0800

    文章新闻的基本接口

---
+++ src/com/sht/restcontroller/ArticleNewsRestController.java
@@ -0,0 +1,120 @@
+package com.sht.restcontroller;
+
+import com.sht.entity.articlenews.ArticleNewsEntity;
+import com.sht.restcontroller.tempentity.AjaxMsg;
+import com.sht.restcontroller.tempentity.UserInfo;
+import com.sht.restcontroller.util.MySessionContext;
+import com.sht.restcontroller.util.UtilEmail;
+import com.sht.restcontroller.util.UtilShtRest;
+import com.sht.service.articlenews.ArticleNewsServiceI;
+import org.apache.commons.lang.StringUtils;
+import org.jeecgframework.core.util.PasswordUtil;
+import org.jeecgframework.core.util.StringUtil;
+import org.jeecgframework.core.util.oConvertUtils;
+import org.jeecgframework.web.system.controller.core.LoginController;
+import org.jeecgframework.web.system.pojo.base.TSUser;
+import org.jeecgframework.web.system.service.MutiLangServiceI;
+import org.jeecgframework.web.system.service.SystemService;
+import org.jeecgframework.web.system.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/article")
+public class ArticleNewsRestController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private SystemService systemService;
+    @Autowired
+    private MutiLangServiceI mutiLangService;
+    @Autowired
+    private ArticleNewsServiceI articleNewsService;
+
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    @ResponseBody
+    public AjaxMsg list(ArticleNewsEntity articleNewsEntity,HttpServletResponse response,HttpServletRequest request){
+        AjaxMsg ajaxMsg = new AjaxMsg();
+        List<ArticleNewsEntity> list = new ArrayList<ArticleNewsEntity>();
+        list = articleNewsService.getList(ArticleNewsEntity.class);
+        //需要分页？
+        ajaxMsg.setMsg("success");
+        ajaxMsg.setResponsecode(HttpStatus.OK.value());
+        ajaxMsg.setModel(list);
+        return ajaxMsg;
+    }
+
+    @RequestMapping(value = "/info/{id}",method = RequestMethod.GET)
+    @ResponseBody
+    public AjaxMsg info(@PathVariable String id, HttpServletResponse response, HttpServletRequest request){
+        AjaxMsg ajaxMsg = new AjaxMsg();
+        ArticleNewsEntity articleNewsEntity = new ArticleNewsEntity();
+        if(StringUtil.isEmpty(id)){
+            ajaxMsg.setMsg("id不能为空");
+            ajaxMsg.setResponsecode(HttpStatus.NOT_FOUND.value());
+        }
+        articleNewsEntity = articleNewsService.getEntity(ArticleNewsEntity.class,id);
+        //需要分页？
+        ajaxMsg.setMsg("success");
+        ajaxMsg.setResponsecode(HttpStatus.OK.value());
+        ajaxMsg.setModel(articleNewsEntity);
+        return ajaxMsg;
+    }
+
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    @ResponseBody
+    public AjaxMsg save(ArticleNewsEntity articleNewsEntity,HttpServletResponse response,HttpServletRequest request){
+        AjaxMsg ajaxMsg = new AjaxMsg();
+        articleNewsService.save(articleNewsEntity);
+        //需要分页？
+        ajaxMsg.setMsg("success");
+        ajaxMsg.setResponsecode(HttpStatus.OK.value());
+        ajaxMsg.setModel(articleNewsEntity);
+        return ajaxMsg;
+    }
+
+    @RequestMapping(value = "/update",method = RequestMethod.POST)
+    @ResponseBody
+    public AjaxMsg update(ArticleNewsEntity articleNewsEntity,HttpServletResponse response,HttpServletRequest request){
+        AjaxMsg ajaxMsg = new AjaxMsg();
+        if(articleNewsEntity==null  || articleNewsEntity.getId()==null){
+            ajaxMsg.setMsg("id不能为空");
+            ajaxMsg.setResponsecode(HttpStatus.OK.value());
+        }
+        articleNewsService.updateEntitie(articleNewsEntity);
+        //需要分页？
+        ajaxMsg.setMsg("success");
+        ajaxMsg.setResponsecode(HttpStatus.OK.value());
+        ajaxMsg.setModel(articleNewsEntity);
+        return ajaxMsg;
+    }
+
+    @RequestMapping(value = "/delete",method = RequestMethod.POST)
+    @ResponseBody
+    public AjaxMsg delete(ArticleNewsEntity articleNewsEntity,HttpServletResponse response,HttpServletRequest request){
+        AjaxMsg ajaxMsg = new AjaxMsg();
+        if(articleNewsEntity==null  || articleNewsEntity.getId()==null){
+            ajaxMsg.setMsg("id不能为空");
+            ajaxMsg.setResponsecode(HttpStatus.OK.value());
+        }
+        articleNewsService.deleteEntityById(ArticleNewsEntity.class,articleNewsEntity.getId());
+        //需要分页？
+        ajaxMsg.setMsg("success");
+        ajaxMsg.setResponsecode(HttpStatus.OK.value());
+        ajaxMsg.setModel(articleNewsEntity);
+        return ajaxMsg;
+    }
+
+}
+
