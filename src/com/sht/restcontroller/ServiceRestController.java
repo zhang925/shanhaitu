26afeec607e9750f7faf0b008807b2f8c5662f20
zhang925 <zhang925518@163.com>
2018-06-14 23:53:11 +0800

    修改

--- src/com/sht/restcontroller/ServiceRestController.java
+++ src/com/sht/restcontroller/ServiceRestController.java
@@ -1,7 +1,9 @@
 package com.sht.restcontroller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.sht.entity.service.ServiceEntity;
 import com.sht.restcontroller.tempentity.AjaxMsg;
+import com.sht.restcontroller.util.UtilSht;
 import org.jeecgframework.core.util.StringUtil;
 import org.jeecgframework.web.system.service.SystemService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/service")
 /**
- * 服务  外部接口
+ * 出海服务  外部接口
  */
 public class ServiceRestController {
 
@@ -35,6 +38,86 @@ public class ServiceRestController {
         ajaxMsg.setMsg("success");
         ajaxMsg.setResponsecode(HttpStatus.OK.value());
         ajaxMsg.setModel(list);
+        return ajaxMsg;
+    }
+
+    /**
+     * 根据业务类型分页获取出海服务的信息
+     * @param serviceEntity
+     * @param response
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/list/info",method = RequestMethod.GET)
+    @ResponseBody
+    public AjaxMsg listInfo(ServiceEntity serviceEntity, HttpServletResponse response, HttpServletRequest request){
+        AjaxMsg ajaxMsg = new AjaxMsg();
+
+        // 说是 查询 用户信息
+        String sql2 = "";
+
+
+        /*SELECT * FROM t_s_user as tsu
+        LEFT JOIN
+        t_s_base_user tsbu
+        ON
+        tsbu.ID = tsu.id
+        LEFT JOIN
+        sht_comment as sc
+        ON tsu.id = sc.use_id
+        LEFT JOIN
+        sht_service as ss
+        ON sc.service_id = ss.id
+        LEFT JOIN
+        sht_bus_type sbt
+        ON  sbt.id = ss.bus_type_id;*/
+
+
+
+
+        String sql = "select * from sht_article_news where 1=1 ";//这里不采用jeecg的方法了，方便以后其他人维护。
+        String countSql = "select count(0) from sht_article_news  where 1=1  ";
+        String where = "";
+        //拼接条件
+        //要根据ID查询
+        String fenleiid = "";//分类ID
+        if(!StringUtil.isEmpty(fenleiid)){//非空
+            where = " and cate_id ='"+fenleiid+"' ";
+        }
+        String title = "";//标题模糊查询
+        if(!StringUtil.isEmpty(title)){//非空
+            where = where + " and title like '%"+title+"%' ";
+        }
+        countSql = countSql + where;
+        where = where + " order by created_time DESC ";
+        sql = sql + where;
+        long num  = systemService.getCountForJdbcParam(countSql,new Object[]{});//总条数
+        Integer page = UtilSht.getPage(request);
+        Integer row = UtilSht.getRow(request);
+
+
+        int total = (int)num;//总条数
+        Integer totalPage = (total%row==0) ? (total/row) : ((total/row)+1);
+        if(page >= totalPage){
+            page = totalPage;
+        }
+        if(page==0){
+            page =1;
+        }
+        if(totalPage == 0){
+            totalPage =1;
+        }
+        List<Map<String, Object>> list2 = systemService.findForJdbc(sql,page,row);
+        JSONObject obj = new JSONObject();
+        obj.put("result", list2);
+        obj.put("total",total);
+        obj.put("totalPage",totalPage);
+        obj.put("page",page);
+        obj.put("row",row);
+
+        ajaxMsg.setMsg("success");
+        ajaxMsg.setResponsecode(HttpStatus.OK.value());
+        ajaxMsg.setModel(obj);
         return ajaxMsg;
     }
 
